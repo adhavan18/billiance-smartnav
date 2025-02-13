f2018ce1df8def86b33c6c864953c625d1cde348
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2025-02-13 08:30:35 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,24 +12,24 @@ const HomePage = () => {
 
   
   const todaysDeals = [
-    { name: 'Deal 1', price: 19.99, image: '/carousel_image1.jpg' },
+    { name: 'Deal 1', price: 19.99, image: '/carousel_image2.jpg' },
     { name: 'Deal 2', price: 29.99, image: '/carousel_image1.jpg' },
-    { name: 'Deal 3', price: 39.99, image: '/carousel_image1.jpg' },
+    { name: 'Deal 3', price: 39.99, image: '/carousel_image3.jpg' },
     { name: 'Deal 4', price: 49.99, image: '/carousel_image1.jpg' },
   ];
 
   const bestSellers = [
-    { name: 'Best Seller 1', price: 24.99, image: '/carousel_image1.jpg' },
-    { name: 'Best Seller 2', price: 34.99, image: '/carousel_image1.jpg' },
+    { name: 'Best Seller 1', price: 24.99, image: '/carousel_image2.jpg' },
+    { name: 'Best Seller 2', price: 34.99, image: '/carousel_image3.jpg' },
     { name: 'Best Seller 3', price: 44.99, image: '/carousel_image1.jpg' },
-    { name: 'Best Seller 4', price: 54.99, image: '/carousel_image1.jpg' },
+    { name: 'Best Seller 4', price: 54.99, image: '/carousel_image2.jpg' },
   ];
 
   const newArrivals = [
-    { name: 'New Arrival 1', price: 14.99, image: '/public/locator.jpg' },
-    { name: 'New Arrival 2', price: 24.99, image: '/images/arrival2.jpg' },
-    { name: 'New Arrival 3', price: 34.99, image: '/images/arrival3.jpg' },
-    { name: 'New Arrival 4', price: 44.99, image: '/images/arrival4.jpg' },
+    { name: 'New Arrival 1', price: 14.99, image: '/carousel_image3.jpg' },
+    { name: 'New Arrival 2', price: 24.99, image: '/carousel_image2.jpg' },
+    { name: 'New Arrival 3', price: 34.99, image: '/carousel_image1.jpg' },
+    { name: 'New Arrival 4', price: 44.99, image: '/carousel_image2.jpg' },
   ];
 
   return (
