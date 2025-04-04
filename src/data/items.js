7b11ce28cf1e31219cc67567bf8748ed75b7f61c
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2025-04-05 03:58:29 +0530

    Update items.js

--- src/data/items.js
+++ src/data/items.js
@@ -40,7 +40,91 @@ export const items = [
       price: "200.00",
       location: "Aisle 5, Section 2",
       image:"/fortune.jpg",
+    },
+    {
+      id: "6",
+      name: "Colgate Toothpaste",
+      expiry: "05/2027",
+      price: "95.00",
+      location: "Aisle 2, Section 3",
+      image: "/colgate.jpg"
+    },
+    {
+      id: "7",
+      name: "Brooke Bond Red Label Tea",
+      expiry: "11/2026",
+      price: "150.00",
+      location: "Aisle 3, Section 1",
+      image: "/red_label.jpg"
+    },
+    {
+      id: "8",
+      name: "Nestlé Everyday Milk Powder",
+      expiry: "08/2029",
+      price: "250.00",
+      location: "Aisle 4, Section 3",
+      image: "/everyday.jpg"
+    },
+    {
+      id: "9",
+      name: "Maggie 2-Minute Noodles",
+      expiry: "03/2027",
+      price: "55.00",
+      location: "Aisle 1, Section 1",
+      image: "/maggie.jpg"
+    },
+    {
+      id: "10",
+      name: "Britannia Cheese Slices",
+      expiry: "07/2026",
+      price: "160.00",
+      location: "Aisle 5, Refrigerated Section",
+      image: "/britannia_cheese.jpg"
+    },
+    {
+      id: "11",
+      name: "Amul Butter",
+      expiry: "06/2026",
+      price: "220.00",
+      location: "Aisle 5, Refrigerated Section",
+      image: "/amul_butter.jpg"
+    },
+    {
+      id: "12",
+      name: "Himalaya Face Wash",
+      expiry: "04/2028",
+      price: "135.00",
+      location: "Aisle 6, Section 2",
+      image: "/himalaya_facewash.jpg"
+    },
+    {
+      id: "13",
+      name: "Tropicana Orange Juice",
+      expiry: "01/2027",
+      price: "110.00",
+      location: "Aisle 5, Refrigerated Section",
+      image: "/tropicana.jpg"
+    },
+    {
+      id: "14",
+      name: "Dettol Hand Wash",
+      expiry: "09/2028",
+      price: "99.00",
+      location: "Aisle 6, Section 1",
+      image: "/dettol_handwash.jpg"
+    },
+    {
+      id: "15",
+      name: "Kissan Mixed Fruit Jam",
+      expiry: "02/2030",
+      price: "135.00",
+      location: "Aisle 3, Section 2",
+      image: "/kissan_jam.jpg"
     }
+  
+  ];
+  
+  
   
   ];
   
